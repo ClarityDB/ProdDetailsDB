8dac3def316aa3c8971757ebae0fc9fb674806f8
Angie davidson <adavidson1@gmail.com>
2020-08-26 17:37:41 -0600

    updated server to include prod list for educationsl reasons

--- server.js
+++ server.js
@@ -10,6 +10,53 @@ app.use(express.static(path.join(__dirname, "./client/public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/products/list', (req, res) => {
+  res.status(200).send(
+    [
+      {
+        "id": 1,
+        "name": "Camo Onesie",
+        "slogan": "Blend in to your crowd",
+        "description": "The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.",
+        "category": "Jackets",
+        "default_price": "140"
+      },
+      {
+        "id": 2,
+        "name": "Bright Future Sunglasses",
+        "slogan": "You've got to wear shades",
+        "description": "Where you're going you might not need roads, but you definitely need some shades. Give those baby blues a rest and let the future shine bright on these timeless lenses.",
+        "category": "Accessories",
+        "default_price": "69"
+      },
+      {
+        "id": 3,
+        "name": "Morning Joggers",
+        "slogan": "Make yourself a morning person",
+        "description": "Whether you're a morning person or not.  Whether you're gym bound or not.  Everyone looks good in joggers.",
+        "category": "Pants",
+        "default_price": "40"
+      },
+      {
+        "id": 4,
+        "name": "Slacker's Slacks",
+        "slogan": "Comfortable for everything, or nothing",
+        "description": "I'll tell you how great they are after I nap for a bit.",
+        "category": "Pants",
+        "default_price": "65"
+      },
+      {
+        "id": 5,
+        "name": "Heir Force Ones",
+        "slogan": "A sneaker dynasty",
+        "description": "Now where da boxes where I keep mine? You should peep mine, maybe once or twice but never three times. I'm just a sneaker pro, I love Pumas and shell toes, but can't nothin compare to a fresh crispy white pearl",
+        "category": "Kicks",
+        "default_price": "99"
+      }
+    ]
+  );
+});
+
 app.get('/products/:product_id', (req, res) => {
   res.status(200).send(
     {
@@ -35,7 +82,7 @@ app.get('/products/:product_id', (req, res) => {
       ]
     }
   );
-})
+});
 
 app.get('/products/:product_id/styles', (req, res) => {
   res.status(200).send(
@@ -290,7 +337,7 @@ app.get('/products/:product_id/styles', (req, res) => {
       ]
     }
   );
-})
+});
 
 app.get('/reviews/:product_id/list', (req, res) => {
   res.status(200).send(
@@ -362,7 +409,7 @@ app.get('/reviews/:product_id/list', (req, res) => {
       ]
     }
   );
-})
+});
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
