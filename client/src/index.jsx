52d41e6f37d6df0888bb9c7c6c0a57f953e2c70c
AngieD8615 <63127566+AngieD8615@users.noreply.github.com>
2020-08-29 14:59:49 -0600

    Revert "server sends information stored in DB to the client"

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,14 @@
-/* eslint-disable */ 
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-import App from './components/App.jsx';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import App from './components/App';
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <Route path="/:productId" component={App} />
+    <Route exact path="/">
+      <Redirect to="/5" />
+    </Route>
+  </Router>,
   document.getElementById('root'),
 );
